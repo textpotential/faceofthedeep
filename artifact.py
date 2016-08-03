28240c25fe4abe2d78eead22158688967060b310
Michael Hemenway <mphemenway@gmail.com>
2016-08-03 18:31:05 +0000

    format a bit and clean up comments

--- artifact.py
+++ artifact.py
@@ -7,6 +7,7 @@ Created on Jul 5, 2016
 import re
 import requests
 
+
 class Artifact(object):
     '''
     classdocs
@@ -20,22 +21,22 @@ class Artifact(object):
         '''
         self.uri = uri
         self.internal = internal
-        self.relationships = [] # list of artifact instances
+        self.relationships = []  # list of artifact instances
         Artifact.refs.append(self)
 
     def relate(self, other):
         self.relationships.append(other)
         other.relationships.append(self)
-        
+
     def score(self, scorer):
         top = scorer(self.uri, self.internal)
         self.relationships = top
-        
+
     def get_features(self):
         # needs lots of expansion, assuming text for now
         response = requests.get(self.uri)
         feats = re.findall(r'\w+|[^\s\w]', response.content.decode())
-        
+
         return feats
 
     @classmethod
@@ -47,7 +48,7 @@ if __name__ == '__main__':
     import string
     from random import choice
     letters = list(string.ascii_letters)
-    
+
     for i in range(1000000):
         url = ''.join(choice(letters) for i in range(50))
         a = Artifact(url)
--- bible_api.py
+++ bible_api.py
@@ -1,8 +1,8 @@
 import requests
-#from pprint import pprint
+
 
 def get_verse(api_token, book, chapt, verse):
-    
+
     endpoint = 'https://bibles.org/v2/versions.js'
     pw_fake = 'X'
 
@@ -11,15 +11,17 @@ def get_verse(api_token, book, chapt, verse):
 
     for version in versions['response']['versions']:
         version_id = version['id']
-        #print(version_id)
-        passage_endpoint = 'https://bibles.org/v2/passages.js?q[]={book}+{chapt}:{verse}&version={versionid}'
-        p_ep = passage_endpoint.format(book=book, chapt=chapt, verse=verse, versionid=version_id)
-        #print(p_ep)
+
+        passage_endpoint = ('https://bibles.org/v2/passages.js?' +
+                            'q[]={book}+{chapt}:{verse}&version={versionid}')
+
+        p_ep = passage_endpoint.format(book=book, chapt=chapt, verse=verse,
+                                       versionid=version_id)
+
         req_pas = requests.get(p_ep, auth=(api_token, pw_fake))
-        #print(req_pas.url)
         passage = req_pas.json()
-        #pprint(passage)
         response = passage.get('response')
+
         if response:
             search = response.get('search')
             if search:
@@ -29,14 +31,3 @@ def get_verse(api_token, book, chapt, verse):
                     if passages:
                         for passage in passages:
                             print(passage['version'], passage['text'])
-#     pas_version = passage['response']['search']['result']['passages']['version']
-#     pas_text = passage['response']['search']['result']['passages']['text']
-#     
-#     print(pas_version, ':', pas_text)
-#     
-
-#     url = 'https://bibles.org/v2/verses/' + version_id + ':' + verse + '.js'
-#     response = requests.get(url, auth=(api_token, pw_fake))
-#     verse = response.json()
-#     print(verse)
-#     
